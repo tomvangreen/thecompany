ea9ab8d58eed1de005ddecff5985eb0caf7a14eb
atombrot@gmail.com <atombrot@gmail.com>
2014-08-24 21:48:07 +0200

    Blarg

--- core/src/ch/digitalmeat/company/level/LayerLoader.java
+++ core/src/ch/digitalmeat/company/level/LayerLoader.java
@@ -5,6 +5,16 @@ import ch.digitalmeat.company.level.Tile.TerrainType;
 import com.badlogic.gdx.graphics.Pixmap;
 
 public class LayerLoader {
+	public boolean yDown;
+
+	public LayerLoader() {
+		this(false);
+	}
+
+	public LayerLoader(boolean yDown) {
+		this.yDown = yDown;
+	}
+
 	public void applyLayer(Map map, Pixmap pixmap, TerrainType type) {
 
 	}
