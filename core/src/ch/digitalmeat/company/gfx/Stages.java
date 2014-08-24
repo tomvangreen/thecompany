b7798386c013948975b039ec19b820f2f7c5098d
atombrot@gmail.com <atombrot@gmail.com>
2014-08-24 23:35:23 +0200

    Map is scrollable (rightclick)

--- core/src/ch/digitalmeat/company/gfx/Stages.java
+++ core/src/ch/digitalmeat/company/gfx/Stages.java
@@ -1,11 +1,16 @@
 package ch.digitalmeat.company.gfx;
 
+import ch.digitalmeat.company.input.MapCamProcessor;
+import ch.digitalmeat.company.level.GameMap;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class Cams {
+public class Stages {
 	public final Viewport gameViewport;
 	public final Viewport uiViewport;
 
@@ -15,7 +20,12 @@ public class Cams {
 	public final Stage game;
 	public final Stage ui;
 
-	public Cams(int width, int height) {
+	private InputMultiplexer plexer = new InputMultiplexer();
+	private InputMultiplexer gamePlexer = new InputMultiplexer();
+	private InputMultiplexer uiPlexer = new InputMultiplexer();
+	private GameMap map;
+
+	public Stages(int width, int height) {
 		gameCam = new OrthographicCamera();
 		gameViewport = new FitViewport(width, height, gameCam);
 		game = new Stage(gameViewport);
@@ -23,6 +33,14 @@ public class Cams {
 		uiCam = new OrthographicCamera();
 		uiViewport = new FitViewport(width, height, uiCam);
 		ui = new Stage(uiViewport);
+
+		plexer.addProcessor(uiPlexer);
+		uiPlexer.addProcessor(ui);
+
+		plexer.addProcessor(gamePlexer);
+		gamePlexer.addProcessor(new MapCamProcessor(gameViewport, gameCam));
+		gamePlexer.addProcessor(game);
+		Gdx.input.setInputProcessor(plexer);
 	}
 
 	public void resize(int width, int height) {
@@ -40,5 +58,18 @@ public class Cams {
 	public void draw() {
 		game.draw();
 		ui.draw();
+	}
+
+	public void loadMap(GameMap map) {
+		unloadMap();
+		this.map = map;
+		game.clear();
+		game.addActor(new MapRenderer(map));
+	}
+
+	public void unloadMap() {
+		if (this.map != null) {
+
+		}
 	}
 }
