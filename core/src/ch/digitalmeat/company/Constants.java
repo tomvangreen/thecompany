ea504ea33da3c3c09f43fab35effbc55fdfcca33
atombrot@gmail.com <atombrot@gmail.com>
2014-08-24 22:31:39 +0200

    Fixed window size

--- core/src/ch/digitalmeat/company/Constants.java
+++ core/src/ch/digitalmeat/company/Constants.java
@@ -3,4 +3,7 @@ package ch.digitalmeat.company;
 public class Constants {
 	public final static int VIRTUAL_WIDTH = 320;
 	public final static int VIRTUAL_HEIGHT = 200;
+
+	public final static int WINDOW_WIDTH = 640;
+	public final static int WINDOW_HEIGHT = 400;
 }
--- desktop/src/ch/digitalmeat/company/desktop/DesktopLauncher.java
+++ desktop/src/ch/digitalmeat/company/desktop/DesktopLauncher.java
@@ -1,12 +1,16 @@
 package ch.digitalmeat.company.desktop;
 
+import ch.digitalmeat.company.App;
+import ch.digitalmeat.company.Constants;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import ch.digitalmeat.company.App;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Constants.WINDOW_WIDTH;
+		config.height = Constants.WINDOW_HEIGHT;
 		new LwjglApplication(new App(), config);
 	}
 }
