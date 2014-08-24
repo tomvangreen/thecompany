680f3d222a0881676b1aef64b25121da6e0f0eba
atombrot@gmail.com <atombrot@gmail.com>
2014-08-24 20:57:03 +0200

    initial commit

---
+++ html/src/ch/digitalmeat/company/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package ch.digitalmeat.company.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import ch.digitalmeat.company.TheCompany;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new TheCompany();
+        }
+}
