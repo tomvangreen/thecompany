6eb5cc3bc7a4bedf9aa102777d502576a329cdf7
atombrot@gmail.com <atombrot@gmail.com>
2014-08-25 04:49:22 +0200

    good type

--- core/src/ch/digitalmeat/company/game/economy/Good.java
+++ core/src/ch/digitalmeat/company/game/economy/Good.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.utils.JsonValue;
 public class Good extends EconomyItem {
 	public float basePrice;
 	public float weight;
+	public GoodType type = GoodType.General;
 
 	public Good(Economy economy) {
 		super(economy);
@@ -15,5 +16,13 @@ public class Good extends EconomyItem {
 	protected void readSpecifics(Json json, JsonValue jsonData) {
 		basePrice = jsonData.getFloat("price");
 		weight = jsonData.getFloat("weight", 1f);
+		if (jsonData.has("type")) {
+			String typeString = jsonData.getString("type");
+			type = GoodType.valueOf(typeString);
+		}
+	}
+
+	public static enum GoodType {
+		General, Food, BuildingMaterial, Weapons
 	}
 }
