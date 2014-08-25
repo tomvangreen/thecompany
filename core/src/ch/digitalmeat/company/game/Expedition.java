e823f491cc38b2f2dd39bcbf7c1adf75c30cdd7d
thobens <thobens@gmail.com>
2014-08-25 14:25:59 +0200

    removed generalisation of expedition units

--- core/src/ch/digitalmeat/company/game/Expedition.java
+++ core/src/ch/digitalmeat/company/game/Expedition.java
@@ -4,7 +4,72 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Expedition {
+
+	public final List<Vehicle> vehicles = new ArrayList<Vehicle>();
 	
-	public final List<ExpeditionUnit> units = new ArrayList<ExpeditionUnit>();
+	public final List<Waypoint> waypoints = new ArrayList<Waypoint>();
 	
+	protected float weight;
+	
+	protected float speed;
+	
+	protected float fightForce;
+	
+	protected float workForce;
+	
+	protected float lineOfSight;
+	
+	public Expedition() {
+		weight = 1f;
+		speed = 1f;
+		fightForce = 1f;
+		workForce = 1f;
+		lineOfSight = 1f;
+	}
+
+	public float getWeight() {
+		weight = 0f;
+		for (Vehicle vehicle : vehicles) {
+			weight += vehicle.weight;
+		}
+		return weight;
+	}
+
+	public float getSpeed() {
+		speed = Float.MAX_VALUE;
+		for (Vehicle vehicle : vehicles) {
+			if(speed < vehicle.speed) {
+				speed = vehicle.speed;
+			}
+		}
+		return speed;
+	}
+
+	public float getFightForce() {
+		fightForce = 0f;
+		for (Vehicle vehicle : vehicles) {
+			fightForce += vehicle.fightForce;
+		}
+		return fightForce;
+	}
+
+	public float getWorkForce() {
+		workForce = 0f;
+		for (Vehicle vehicle : vehicles) {
+			workForce += vehicle.workForce;
+		}
+		return workForce;
+	}
+	
+	public float getLineOfSight() {
+		lineOfSight = 0f;
+		for (Vehicle vehicle : vehicles) {
+			float tmpLos= vehicle.lineOfSight;
+			if(lineOfSight < tmpLos) {
+				lineOfSight = tmpLos;
+			}
+		}
+		return lineOfSight;
+	}
+
 }
--- core/src/ch/digitalmeat/company/game/Person.java
+++ core/src/ch/digitalmeat/company/game/Person.java
@@ -6,15 +6,34 @@ import java.util.List;
 import ch.digitalmeat.company.game.economy.GoodComponent;
 import ch.digitalmeat.company.game.economy.Profession;
 
-public class Person extends BaseExpeditionUnit {
+public class Person {
 	
 	public final List<Profession> professions = new ArrayList<Profession>();
 	
-	public float happiness = 0f;
+	public float happiness;
 	
 	public PersonType personType = PersonType.Apprentice;
 	
 	public GoodComponent consumption;
+	
+	public float weight;
+	
+	public float speed;
+	
+	public float fightForce;
+	
+	public float workForce;
+	
+	public float lineOfSight;
+
+	public Person() {
+		weight = 1f;
+		speed = 1f;
+		fightForce = 1f;
+		workForce = 1f;
+		lineOfSight = 1f;
+		happiness = 1f;
+	}
 
 	public static enum PersonType {
 		// @formatter: off
